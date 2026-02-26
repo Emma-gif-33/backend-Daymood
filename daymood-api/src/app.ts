c32e9e7a82466a4ceda2edc4b1cfa43a426d6a88
naddiluv <nadiaguerrc@gmail.com>
2026-02-26 02:05:48 -0600

    token received correctly

--- daymood-api/src/app.ts
+++ daymood-api/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { verifyToken, AuthRequest } from './middlewares/auth.middleware';
+import { Response } from 'express';
 
 const app = express();
 
@@ -16,6 +18,29 @@ app.get('/', (req, res) => {
     res.json({ message: 'Si jala' });
 });
 
+// Ruta de prueba — verifica que el token funciona
+app.get('/test-token', verifyToken, (req: AuthRequest, res: Response) => {
+    res.json({
+        success: true,
+        message: 'Token válido',
+        uid: req.user!.uid,
+        email: req.user!.email
+    });
+});
+
+
+app.post('/auth/register', verifyToken, (req: AuthRequest, res: Response) => {
+    console.log('UID del token:', req.user!.uid);
+    console.log('Body recibido:', req.body);
+
+    res.json({
+        success: true,
+        message: 'Token verificado correctamente',
+        uid: req.user!.uid,
+        email: req.user!.email,
+        bodyRecibido: req.body
+    });
+});
 // Catch-all para rutas no encontradas
 app.use((req, res) => {
     console.log(`Ruta no encontrada: ${req.method} ${req.path}`);
