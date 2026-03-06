d10dd1d879fc66d3707da719643ae7a3f5b17f54
Emma <reyespreciadoemma@gmail.com>
2026-03-06 10:36:51 -0600

    Correction. Forms GetById.

--- daymood-api/src/server.ts
+++ daymood-api/src/server.ts
@@ -5,10 +5,4 @@ const PORT = 3000;
 
 app.listen(PORT, async () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-
-    // try {
-    //     await insertSnapshot();
-    // } catch (error: any) {
-    //     console.error('❌ Error enviando snapshot inicial a BigQuery:', error.message);
-    // }
 });
